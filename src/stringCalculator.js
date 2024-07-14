2d92985f141c74321b4a15b6673060c4d9307940
ganeshpandi <heythisisgp@gmail.com>
2024-07-15 00:48:19 +0530

    Update stringCalculator.js

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,30 +1,31 @@
-class StringCalculator {
-    add(numbers) {
-        if (numbers === '') return 0;
+function add(numbers) {
+  if (numbers === "") return 0;
 
-        let delimiter = /[\n,]/;
-        if (numbers.startsWith('//')) {
-            const parts = numbers.split('\n', 2);
-            delimiter = new RegExp(parts[0].substring(2));
-            numbers = parts[1];
-        }
+  let delimiter = ",";
+  let customDelimiter = numbers.match(/^\/\/([^]*)\n/);
+  if (customDelimiter) {
+    delimiter = customDelimiter[1];
+    numbers = numbers.replace(/^\/\/[^]*\n/, "");
+  }
 
-        const parts = numbers.split(delimiter);
-        const negatives = [];
-        const sum = parts.reduce((sum, num) => {
-            const number = parseInt(num, 10);
-            if (number < 0) negatives.push(number);
-            if (number <= 1000) return sum + number;
-            return sum;
-        }, 0);
+  let numArray = numbers.split(new RegExp(`[${delimiter}\n]`));
+  let negatives = [];
+  let sum = 0;
 
-        if (negatives.length > 0) {
-            throw new Error(`negatives not allowed: ${negatives.join(',')}`);
-        }
-
-        return sum;
+  for (let num of numArray) {
+    if (num !== "") {
+      let intNum = parseInt(num, 10);
+      if (intNum < 0) {
+        negatives.push(intNum);
+      } else if (intNum <= 1000) {
+        sum += intNum;
+      }
     }
+  }
+
+  if (negatives.length > 0) {
+    throw new Error(`Negatives not allowed: ${negatives.join(", ")}`);
+  }
+
+  return sum;
 }
-
-module.exports = StringCalculator;
-
