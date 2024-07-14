635f00e32f25ee3607df0ea0454652c030c50542
ganeshpandi <heythisisgp@gmail.com>
2024-07-15 00:52:14 +0530

    Update stringCalculator.js

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,31 +1,34 @@
-function add(numbers) {
-  if (numbers === "") return 0;
+class StringCalc {
+    static addition(numbers) {
+        if (numbers === '') {
+            return 0;
+        }
 
-  let delimiter = ",";
-  let customDelimiter = numbers.match(/^\/\/([^]*)\n/);
-  if (customDelimiter) {
-    delimiter = customDelimiter[1];
-    numbers = numbers.replace(/^\/\/[^]*\n/, "");
-  }
+        let delimiters = [',', '\n'];
+        let customMatch = numbers.match(/^\/\/(\[.*?\])+\n/);
+        
+        if (customMatch) {
+            let delimiterPart = customMatch[0];
+            let delimiterPattern = /(\[.*?\])/g;
+            let match;
 
-  let numArray = numbers.split(new RegExp(`[${delimiter}\n]`));
-  let negatives = [];
-  let sum = 0;
+            while (match = delimiterPattern.exec(delimiterPart)) {
+                delimiters.push(match[1].slice(1, -1));
+            }
 
-  for (let num of numArray) {
-    if (num !== "") {
-      let intNum = parseInt(num, 10);
-      if (intNum < 0) {
-        negatives.push(intNum);
-      } else if (intNum <= 1000) {
-        sum += intNum;
-      }
+            numbers = numbers.slice(customMatch[0].length);
+        }
+
+        let delimiterRegex = new RegExp('[' + delimiters.map(d => d.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')).join('') + ']');
+        let numberArray = numbers.split(delimiterRegex).map(num => parseInt(num, 10));
+        
+        let negativesValues = numberArray.filter(num => num < 0);
+        if (negativesValues.length > 0) {
+            throw new Error(`Negatives are not allowed: ${negativesValues.join(', ')}`);
+        }
+
+        return numberArray
+            .filter(num => num <= 1000)
+            .reduce((sum, num) => sum + (isNaN(num) ? 0 : num), 0);
     }
-  }
-
-  if (negatives.length > 0) {
-    throw new Error(`Negatives not allowed: ${negatives.join(", ")}`);
-  }
-
-  return sum;
 }
